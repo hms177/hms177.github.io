4157b7dac742813af9031129e79cc341c9faa827
miseon <97710466+hms177@users.noreply.github.com>
2023-02-07 18:30:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,24 @@ setInterval(getTime, 1000);
 
 
 
+/* login */
+const loginForm = document.querySelector(".login-form");
+const loginInput = document.querySelector(".login-form input"); 
+const greeting = document.querySelector(".greeting");
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(e){
+    e.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = "Hello " + username;
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+
+
 /* TO DO */
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
